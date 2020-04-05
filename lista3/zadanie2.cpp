1fc6420057f6a6262e3d79c1686d0ca09e98be2a
HoopDead <polec0@wp.pl>
2020-04-05 16:29:26 +0200

    Poprawiona lista 2 i 3

--- lista3/zadanie2.cpp
+++ lista3/zadanie2.cpp
@@ -2,44 +2,22 @@
 
 using namespace std;
 
-double tablica_a[100];
-
-double fillTablica_a() {
-    for(int i = 0; i < 100; i++)
-    {
-        tablica_a[i] = i;
-    }
-}
-
-double f(double* a, int b)
-{
-    return 1.0;
-}
-
-void g(int *a)
-{
-    *a = 5;
-}
-
 int main()
 {
-    //Przykład A
-    fillTablica_a();
-    double *wska = tablica_a;
-    cout << wska << "\n";
-    //Przykład B
-    char *tablica_b[5][5];
-    //Przykład C
-    int x;
-    char *const tablica_c = new char[x];
-    //Przykład D
-    vector<void*> t;
-    //Przykład E
-    vector<double>tablica_e[4]; 
-    //Przykład F
-    double (*f)(double*,int);
-    //Przykłąd G
-    int y = 0;
-    g(&y);
+    //A
+    double(*x)[100];
+    //B
+    char * array[5][5];
+    //C
+    char *array[];
+    //D
+    vector <void*> wektor;
+    //E
+    vector <double> wektor[5];
+    //F
+    double (*func)(double*, int)
+    //G
+    int *a;
+    int funkcja(*a);
     return 0;
 }
--- lista3/zadanie3.cpp
+++ lista3/zadanie3.cpp
@@ -5,16 +5,27 @@ using namespace std;
 
 int main()
 {
-    auto t1 = chrono::high_resolution_clock::now();
+    //NEW_DELETE
+    auto t_wskazniki1 = chrono::high_resolution_clock::now();
     for(unsigned n = 1; n <= 4000000; ++n)
     {
-        auto start = chrono::high_resolution_clock::now();
         int *p = new int[n];
         delete [] p;
-        auto end = chrono::high_resolution_clock::now();
     }
-    auto t2 = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
-    cout << "\n" << duration << "\n";
+    auto t_wskazniki2 = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(t_wskazniki2 - t_wskazniki1).count();
+    
+    //DODAWANIE
+    int x = 0;
+    auto t_dodawanie1 = chrono::high_resolution_clock::now();
+    for (unsigned n = 1; n < 4000000; ++n)
+    {
+        x = x + 50;
+    }
+    auto t_dodawanie2 = chrono::high_resolution_clock::now();
+    auto duration2 = chrono::duration_cast<chrono::microseconds>(t_dodawanie2 - t_dodawanie1).count();
+    cout << "Czas new/delete " << duration << "\n";
+    cout << "Czas dodawanie " << duration2 << "\n";
+    //Program do dodawania jest dużo szybszy, niż ten z new/delete.
     return 0;
 }
