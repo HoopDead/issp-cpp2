2135197feaeb4bf76448076321bbeafd4e6f7e1f
HoopDead <polec0@wp.pl>
2020-04-06 19:10:56 +0200

    Lista 4

---
+++ lista4/Samochod.h
@@ -0,0 +1,16 @@
+#include <iostream>
+class Samochod
+{
+
+  private:
+
+    int _sruby;
+    int _kola;
+
+  public:
+    Samochod (int n)
+    : _kola (n), _sruby (4*n)
+    {
+      std::cout << "Samochód z " << _kola << " kolami i " << _sruby << " srubami \n";
+    }
+};
---
+++ lista4/zadanie5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Samochod.h"
+
+using namespace std;
+
+int main()
+{
+    Samochod s4 (4);
+    return 0;
+}
