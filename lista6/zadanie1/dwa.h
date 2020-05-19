cfc10c0f14104e49e4b6f18301fe3a09c2b6c9cb
HoopDead <polec0@wp.pl>
2020-05-19 11:49:05 +0200

    lista 6

---
+++ lista6/zadanie1/dwa.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Dwa {
+    public:
+        Dwa() {
+            cout << " Kolejnosc ";
+        }
+        ~Dwa() {
+            cout << endl;
+        }
+};
---
+++ zajecia/zadanie1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 5;
+    int *y = nullptr;
+    (nullptr) = ?;
+    cout << y;
+    return 0;
+}
